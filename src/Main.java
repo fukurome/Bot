525e8c23abd367b690d485bf648b2f2006d5125c
Alexandra <alexandrafilch@yandex.ru>
2022-10-08 12:01:02 +0500

    cosmetics

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,11 @@ import bot.*;
 public class Main {
     public static void main(String[] args) {
         String user_ID = System.getProperty("user.name");
-        BotLogic Bot = new BotLogic();
-        Bot.sayHello(user_ID);
+        BotLogic bot = new BotLogic();
+        bot.sayHello(user_ID);
         while (true) {
-            String message = Bot.getMessage();
-            String reply = Bot.getReply(message, user_ID);
+            String message = bot.getMessage();
+            String reply = bot.getReply(message, user_ID);
             System.out.println(reply);
         }
     }
